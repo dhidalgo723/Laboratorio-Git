161648acc82e8d9205ee9e74cc055adc7f1dfed6
Mateo Carter <shakesmateocourse@gmail.com>
2026-02-13 16:36:01 +0100

    Creada clase Alumno y metodo afegirNota

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,7 +1,16 @@
 
-public class Main {
 
+public class Main {
+public static String[] alumnos = new String[5];
+public static int[] notas =new int[5];
     public static void main(String[] args) throws Exception {
         System.out.println("Bienvenido al Calculador de Notas");
     }
-}
+
+    public static void afegirnNota(int assignatura, int notaNova, Alumno a){
+            a.setNotas(assignatura, notaNova);
+        }
+    }
+
+
+
