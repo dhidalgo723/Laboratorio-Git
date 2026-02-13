520bbe939d7741d6637f2ac47d8a11f8c54e9fe0
Daniel Hidalgo <daniel.hidalgo.mendez31@gmail.com>
2026-02-13 16:51:18 +0100

    agregada funcion de calcular media

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,18 +1,6 @@
 
 public class Main {
 
-    protected static String[] alumnos = new String[5];
-    protected static int[] notas;
-
-    public static int CalcularMitjana(String nom, int assignatura, int novaNota, int mitjana) {
-        int i = 0;
-        for (i = 0; i < notas.length; i++) {
-            mitjana += notas[i];
-        }
-        mitjana = mitjana / i;
-        return mitjana;
-    }
-
     public static void main(String[] args) throws Exception {
         System.out.println("Bienvenido al Calculador de Notas");
     }
